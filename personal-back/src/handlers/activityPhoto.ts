0789a10067608e62ba8e646614b084ffdd4b3fd9
Gilberto castaño <gilberto.castano@outlook.es>
2023-10-13 16:18:15 -0500

    get photo by id working

--- personal-back/src/handlers/activityPhoto.ts
+++ personal-back/src/handlers/activityPhoto.ts
@@ -31,18 +31,16 @@ export const getAllPhotos = async (req: Request, res: Response) => {
     try {
         const response = await Photo.findAll({
             include: [
-              {
+                {
                 model: Aircraft,
                 attributes: ['id', 'registration', 'special_livery', 'type', 'aircraft_description']
-              },
-              {
+                },
+                {
                 model: Airport,
                 attributes: ['id', 'name', 'code']
-              },
+                },
             ]
-          });
-          
-          
+            });
         res.status(200).json(response)
     } catch (error) {
         res.status(400).json({ error: (error as Error).message });
@@ -66,6 +64,36 @@ export const patchPhoto = async (req: Request, res: Response) => {
             link
         )
         res.status(200).json(response)
+    } catch (error) {
+        res.status(400).json({ error: (error as Error).message });
+    }
+}
+
+export const getPhotoById = async (req: Request, res: Response) => {
+    try {
+        const id = req.params.id
+        if(id){
+            const photoById = await Photo.findByPk(id, {
+                include: [
+                    {
+                    model: Aircraft,
+                    attributes: ['id', 'registration', 'special_livery', 'type', 'aircraft_description']
+                    },
+                    {
+                    model: Airport,
+                    attributes: ['id', 'name', 'code']
+                    },
+                ]
+                })
+            if(photoById){
+                photoById.views += 1;
+                await photoById.save();
+                res.status(200).json(photoById)
+            }else{
+                throw new Error(`photo with id ${id} not found`)
+            }
+            
+        }
     } catch (error) {
         res.status(400).json({ error: (error as Error).message });
     }
--- personal-back/src/routes/photoRouter.ts
+++ personal-back/src/routes/photoRouter.ts
@@ -2,13 +2,15 @@ import { Router } from "express";
 const {
     postPhoto,
     getAllPhotos,
-    patchPhoto
+    patchPhoto,
+    getPhotoById
 } = require ("../handlers/activityPhoto")
 
 const photoRouter = Router();
 
 photoRouter.post("/", postPhoto);
 photoRouter.get("/", getAllPhotos);
+photoRouter.get("/:id", getPhotoById);
 photoRouter.patch("/:id", patchPhoto)
 
 export default photoRouter
